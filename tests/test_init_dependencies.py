40eb3ac06180d39a4b540bd196a8e111160e5e14
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-07-08 01:06:17 +0100

    add langchain & dependencies

--- tests/test_init_dependencies.py
+++ tests/test_init_dependencies.py
@@ -1,5 +1,6 @@
 import pytest
 import logging
+import os
 
 ### Initialise Configuration ###
 
@@ -8,11 +9,19 @@ def test_successful_imports(caplog):
     """Check the imports"""
     try:
         import requests
+        import pytest
+        import langchain
+        import chromadb
+        import openai
     except ImportError as err:
         logging.error("couldn't import a package: %s", err)
 
     for log in caplog.records:
         assert log.levelno < logging.ERROR
+
+
+def test_environs_set(caplog):
+    assert os.getenv("OPENAI_TOKEN") is not None
 
 
 @pytest.mark.skip
